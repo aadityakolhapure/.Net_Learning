3ea8b921c215cd264347a490200f9ae39329863f
Aaditya Kolhapure <aadityakolhapure28@gmail.com>
2025-09-16 16:59:37 +0530

    learn about custom validattion

---
+++ Day7/Day7/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Day7.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:CustomValidator ID="CustomValidator1" runat="server" ErrorMessage="enter the palindrome number" ControlToValidate="TextBox1" Display="Dynamic" ForeColor="Red" OnServerValidate="CustomValidator1_ServerValidate" SetFocusOnError="True"></asp:CustomValidator>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Submit" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Day7/Day7/index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Day7
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            int number = int.Parse( args.Value);
+            int original = number;
+            int reverse=0;
+
+            while(number != 0)
+            {
+                int digit = number % 10;
+                reverse = reverse * 10 + digit;
+                number = number / 10;
+            }if (reverse == original)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid=false;
+            }
+
+        }
+    }
+}
