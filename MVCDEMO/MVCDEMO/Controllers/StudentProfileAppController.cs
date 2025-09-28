22a3cce5cddc087a40d232c33b0e3b1c1c53aa0b
Aaditya Kolhapure <aadityakolhapure28@gmail.com>
2025-09-28 21:50:57 +0530

    mvc folder added

---
+++ MVCDEMO/MVCDEMO/Controllers/StudentProfileAppController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace StudentProfileApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Profile()
+        {
+            // Passing data using ViewBag
+            Session["Name"] = "Aaditya Kolhapure";
+
+            // Passing data using ViewData
+            Session["Course"] = "Computer Science";
+
+            // Using Session
+            Session["Age"] = 21;
+
+            return View();
+        }
+    }
+}
